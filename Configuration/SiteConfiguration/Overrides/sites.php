38a555e1cff8685301cc9b8fe43ed977aa3d3284
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2019-09-26 01:26:35 +0200

    [FEATURE] Possibility to select a theme in the site configuration

---
+++ Configuration/SiteConfiguration/Overrides/sites.php
@@ -0,0 +1,32 @@
+<?php
+
+(function ($extensionKey) {
+    $llPrefix = 'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/SiteConfiguration.xlf:';
+    $themePath = 'EXT:' . $extensionKey . '/Resources/Public/Vendor/PrismJs/themes/';
+
+    $GLOBALS['SiteConfiguration']['site']['columns']['codehighlightTheme'] = [
+        'label' => $llPrefix . 'codehighlightTheme',
+        'config' => [
+            'type' => 'input',
+            'eval' => 'trim',
+            'valuePicker' => [
+                'items' => [
+                    [$llPrefix . 'codehighlightTheme.valuePicker.default', $themePath . 'prism.css'],
+                    ['Coy', $themePath . 'prism-coy.css'],
+                    ['Dark', $themePath . 'prism-dark.css'],
+                    ['Funky', $themePath . 'prism-funky.css'],
+                    ['Okaidia', $themePath . 'prism-okaidia.css'],
+                    ['Solarized Light', $themePath . 'prism-solarizedlight.css'],
+                    ['Tomorrow Night', $themePath . 'prism-tomorrow.css'],
+                    ['Twilight', $themePath . 'prism-twilight.css'],
+                ],
+            ],
+        ],
+    ];
+
+    $GLOBALS['SiteConfiguration']['site']['types']['0']['showitem'] .= '
+    ,
+    --div--;Code Highlight,
+    codehighlightTheme,
+';
+})('codehighlight');
