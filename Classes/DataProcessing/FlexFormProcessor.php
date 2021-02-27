eb18c91ec68bce143d01e93aaebe1883fc1626d6
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2021-02-27 18:34:15 +0100

    [TASK] Use FlexFormProcessor delivered with TYPO3 if available

--- Classes/DataProcessing/FlexFormProcessor.php
+++ Classes/DataProcessing/FlexFormProcessor.php
@@ -16,46 +16,53 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
 
-/**
- * This data processor can be used for processing data for the content elements which have flexform contents in one field
- *
- * Example TypoScript configuration:
- * 10 = Brotkrueml\CodeHighlight\DataProcessing\FlexFormProcessor
- * 10 {
- *   if.isTrue.field = pi_flexform
- *   fieldName = pi_flexform
- *   as = flexform
- * }
- *
- * whereas "flexform" can be used as a variable {flexform} inside Fluid to fetch values.
- *
- * @internal
- */
-class FlexFormProcessor implements DataProcessorInterface
-{
-    /** @var FlexFormService */
-    private $flexFormService;
-
-    public function __construct(FlexFormService $flexFormService = null)
+if (\class_exists(\TYPO3\CMS\Frontend\DataProcessing\FlexFormProcessor::class)) {
+    // since TYPO3 v11.1
+    class FlexFormProcessor extends \TYPO3\CMS\Frontend\DataProcessing\FlexFormProcessor
     {
-        $this->flexFormService = $flexFormService ?? GeneralUtility::makeInstance(FlexFormService::class);
     }
+} else {
+    /**
+     * This data processor can be used for processing data for the content elements which have flexform contents in one field
+     *
+     * Example TypoScript configuration:
+     * 10 = Brotkrueml\CodeHighlight\DataProcessing\FlexFormProcessor
+     * 10 {
+     *   if.isTrue.field = pi_flexform
+     *   fieldName = pi_flexform
+     *   as = flexform
+     * }
+     *
+     * whereas "flexform" can be used as a variable {flexform} inside Fluid to fetch values.
+     *
+     * @internal
+     */
+    class FlexFormProcessor implements DataProcessorInterface
+    {
+        /** @var FlexFormService */
+        private $flexFormService;
 
-    public function process(
-        ContentObjectRenderer $contentObjectRenderer,
-        array $contentObjectConfiguration,
-        array $processorConfiguration,
-        array $processedData
-    ): array {
-        if (isset($processorConfiguration['if.']) && !$contentObjectRenderer->checkIf($processorConfiguration['if.'])) {
-            return $processedData;
+        public function __construct(FlexFormService $flexFormService = null)
+        {
+            $this->flexFormService = $flexFormService ?? GeneralUtility::makeInstance(FlexFormService::class);
         }
 
-        $targetVariableName = $contentObjectRenderer->stdWrapValue('as', $processorConfiguration, 'flexform');
-        $fieldName = $contentObjectRenderer->stdWrapValue('fieldName', $processorConfiguration, 'pi_flexform');
+        public function process(
+            ContentObjectRenderer $contentObjectRenderer,
+            array $contentObjectConfiguration,
+            array $processorConfiguration,
+            array $processedData
+        ): array {
+            if (isset($processorConfiguration['if.']) && !$contentObjectRenderer->checkIf($processorConfiguration['if.'])) {
+                return $processedData;
+            }
 
-        $processedData[$targetVariableName] = $this->flexFormService->convertFlexFormContentToArray($contentObjectRenderer->data[$fieldName]);
+            $targetVariableName = $contentObjectRenderer->stdWrapValue('as', $processorConfiguration, 'flexform');
+            $fieldName = $contentObjectRenderer->stdWrapValue('fieldName', $processorConfiguration, 'pi_flexform');
 
-        return $processedData;
+            $processedData[$targetVariableName] = $this->flexFormService->convertFlexFormContentToArray($contentObjectRenderer->data[$fieldName]);
+
+            return $processedData;
+        }
     }
 }
