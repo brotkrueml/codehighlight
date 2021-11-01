7fa92cf7c5de3fd256f9cfd8116610c485c749af
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2021-11-01 10:40:23 +0100

    [TASK] Use path references from Extension class

--- Configuration/SiteConfiguration/Overrides/sites.php
+++ Configuration/SiteConfiguration/Overrides/sites.php
@@ -9,71 +9,66 @@ declare(strict_types=1);
  * LICENSE.txt file that was distributed with this source code.
  */
 
-(static function ($extensionKey) {
-    $llPrefix = 'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/SiteConfiguration.xlf:';
-    $themePath = 'EXT:' . $extensionKey . '/Resources/Public/Prism/themes/';
-
-    $GLOBALS['SiteConfiguration']['site']['columns'] += [
-        'codehighlightTheme' => [
-            'label' => $llPrefix . 'codehighlightTheme',
-            'config' => [
-                'type' => 'input',
-                'eval' => 'trim',
-                'valuePicker' => [
-                    'items' => [
-                        [$llPrefix . 'codehighlightTheme.valuePicker.default', $themePath . 'prism.css'],
-                        ['Coy', $themePath . 'prism-coy.css'],
-                        ['Dark', $themePath . 'prism-dark.css'],
-                        ['Funky', $themePath . 'prism-funky.css'],
-                        ['Okaidia', $themePath . 'prism-okaidia.css'],
-                        ['Solarized Light', $themePath . 'prism-solarizedlight.css'],
-                        ['Tomorrow Night', $themePath . 'prism-tomorrow.css'],
-                        ['Twilight', $themePath . 'prism-twilight.css'],
-                    ],
-                ],
-            ],
-        ],
-        'codehighlightUseUrlHash' => [
-            'label' => $llPrefix . 'codehighlightUseUrlHash',
-            'description' => $llPrefix . 'codehighlightUseUrlHash.description',
-            'config' => [
-                'type' => 'check',
-                'renderType' => 'checkboxToggle',
+$GLOBALS['SiteConfiguration']['site']['columns'] += [
+    'codehighlightTheme' => [
+        'label' => Brotkrueml\CodeHighlight\Extension::LANGUAGE_PATH_SITE_CONFIGURATION . ':codehighlightTheme',
+        'config' => [
+            'type' => 'input',
+            'eval' => 'trim',
+            'valuePicker' => [
                 'items' => [
-                    [
-                        0 => '',
-                        1 => '',
-                    ],
+                    [Brotkrueml\CodeHighlight\Extension::LANGUAGE_PATH_SITE_CONFIGURATION . ':codehighlightTheme.valuePicker.default', Brotkrueml\CodeHighlight\Extension::THEMES_PATH . 'prism.css'],
+                    ['Coy', Brotkrueml\CodeHighlight\Extension::THEMES_PATH . 'prism-coy.css'],
+                    ['Dark', Brotkrueml\CodeHighlight\Extension::THEMES_PATH . 'prism-dark.css'],
+                    ['Funky', Brotkrueml\CodeHighlight\Extension::THEMES_PATH . 'prism-funky.css'],
+                    ['Okaidia', Brotkrueml\CodeHighlight\Extension::THEMES_PATH . 'prism-okaidia.css'],
+                    ['Solarized Light', Brotkrueml\CodeHighlight\Extension::THEMES_PATH . 'prism-solarizedlight.css'],
+                    ['Tomorrow Night', Brotkrueml\CodeHighlight\Extension::THEMES_PATH . 'prism-tomorrow.css'],
+                    ['Twilight', Brotkrueml\CodeHighlight\Extension::THEMES_PATH . 'prism-twilight.css'],
                 ],
             ],
         ],
-        'codehighlightCommandLineDefaultHost' => [
-            'label' => $llPrefix . 'codehighlightCommandLineDefaultHost',
-            'config' => [
-                'type' => 'input',
-                'eval' => 'trim',
+    ],
+    'codehighlightUseUrlHash' => [
+        'label' => Brotkrueml\CodeHighlight\Extension::LANGUAGE_PATH_SITE_CONFIGURATION . ':codehighlightUseUrlHash',
+        'description' => Brotkrueml\CodeHighlight\Extension::LANGUAGE_PATH_SITE_CONFIGURATION . ':codehighlightUseUrlHash.description',
+        'config' => [
+            'type' => 'check',
+            'renderType' => 'checkboxToggle',
+            'items' => [
+                [
+                    0 => '',
+                    1 => '',
+                ],
             ],
         ],
-        'codehighlightCommandLineDefaultUser' => [
-            'label' => $llPrefix . 'codehighlightCommandLineDefaultUser',
-            'config' => [
-                'type' => 'input',
-                'eval' => 'trim',
-            ],
+    ],
+    'codehighlightCommandLineDefaultHost' => [
+        'label' => Brotkrueml\CodeHighlight\Extension::LANGUAGE_PATH_SITE_CONFIGURATION . ':codehighlightCommandLineDefaultHost',
+        'config' => [
+            'type' => 'input',
+            'eval' => 'trim',
         ],
-    ];
-
-    $GLOBALS['SiteConfiguration']['site']['types']['0']['showitem'] .= '
-        ,
-        --div--;Code Highlight,
-            codehighlightTheme,
-            codehighlightUseUrlHash,
-            --palette--;' . $llPrefix . 'codehighlightCommandLine;codehighlightCommandLine,
-    ';
-
-    $GLOBALS['SiteConfiguration']['site']['palettes'] += [
-        'codehighlightCommandLine' => [
-            'showitem' => 'codehighlightCommandLineDefaultHost,codehighlightCommandLineDefaultUser',
+    ],
+    'codehighlightCommandLineDefaultUser' => [
+        'label' => Brotkrueml\CodeHighlight\Extension::LANGUAGE_PATH_SITE_CONFIGURATION . ':codehighlightCommandLineDefaultUser',
+        'config' => [
+            'type' => 'input',
+            'eval' => 'trim',
         ],
-    ];
-})('codehighlight');
+    ],
+];
+
+$GLOBALS['SiteConfiguration']['site']['types']['0']['showitem'] .= '
+    ,
+    --div--;Code Highlight,
+        codehighlightTheme,
+        codehighlightUseUrlHash,
+        --palette--;' . Brotkrueml\CodeHighlight\Extension::LANGUAGE_PATH_SITE_CONFIGURATION . ':codehighlightCommandLine;codehighlightCommandLine,
+';
+
+$GLOBALS['SiteConfiguration']['site']['palettes'] += [
+    'codehighlightCommandLine' => [
+        'showitem' => 'codehighlightCommandLineDefaultHost,codehighlightCommandLineDefaultUser',
+    ],
+];
