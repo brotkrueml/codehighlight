603e23d20a73597fffe80fcbdd62fa5f4abae72f
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2022-09-24 14:18:39 +0200

    [TASK] Use automatic page TSconfig inclusion for TYPO3 v12
    See: https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Feature-96614-AutomaticInclusionOfPageTsConfigOfExtensions.html
    
    Related: #35

--- ext_tables.php
+++ ext_tables.php
@@ -1,9 +1,16 @@
 <?php
+
+use Brotkrueml\CodeHighlight\Extension;
+use Brotkrueml\CodeHighlight\Hooks\PageLayoutView\ContentElementPreviewRenderer;
+use TYPO3\CMS\Core\Information\Typo3Version;
+
 defined('TYPO3') || die();
 
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '@import "EXT:' . Brotkrueml\CodeHighlight\Extension::KEY . '/Configuration/TSconfig/Page/*.tsconfig"'
-);
+if ((new Typo3Version())->getMajorVersion() < 12) {
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        '@import "EXT:' . Extension::KEY . '/Configuration/TSconfig/Page/*.tsconfig"'
+    );
+}
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['codehighlight'] =
-    Brotkrueml\CodeHighlight\Hooks\PageLayoutView\ContentElementPreviewRenderer::class;
+    ContentElementPreviewRenderer::class;
