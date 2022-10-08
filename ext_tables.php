9b5f135e03f663d477e7dea9728b8298a3702c4f
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2022-10-08 16:15:43 +0200

    [TASK] Rename TSconfig folder to TsConfig

--- ext_tables.php
+++ ext_tables.php
@@ -2,11 +2,12 @@
 
 use Brotkrueml\CodeHighlight\Extension;
 use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') || die();
 
 if ((new Typo3Version())->getMajorVersion() < 12) {
-    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-        '@import "EXT:' . Extension::KEY . '/Configuration/TSconfig/Page/*.tsconfig"'
+    ExtensionManagementUtility::addPageTSConfig(
+        '@import "EXT:' . Extension::KEY . '/Configuration/TsConfig/Page/*.tsconfig"'
     );
 }
